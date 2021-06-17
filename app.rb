64efbd7b2907f5951360f8d4f352805c2c4ddac4
JooMin Choi <joominchoi23@gmail.com>
2021-06-17 17:05:43 +0100

    step 10: creating bookmarks

--- app.rb
+++ app.rb
@@ -13,7 +13,16 @@ class BookmarkManager < Sinatra::Base
 
   get '/bookmarks' do
     @bookmarks = Bookmark.all
-    erb :'bookmarks/index'
+    erb :"bookmarks/index"
+  end
+
+  get '/bookmarks/new' do
+    erb :"bookmarks/new"
+  end
+  
+  post '/bookmarks' do
+    Bookmark.create(url: params[:url])
+    redirect '/bookmarks'
   end
 
   run! if app_file == $0
--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -6,12 +6,3 @@ def setup_test_database
   # Clean the bookmarks table
   connection.exec("TRUNCATE bookmarks;")
 end
-
-# require 'pg'
-
-# p "Setting up test database..."
-
-# connection = PG.connect(dbname: 'bookmark_manager_test')
-
-# # Clear the bookmarks table
-# connection.exec("TRUNCATE bookmarks;")
