dd1248e19be43842d080b752ac930644dadc64c9
JooMin Choi <joominchoi23@gmail.com>
2021-06-18 16:10:51 +0100

    step 11: wrapping data in program objects

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,4 +1,5 @@
 require 'bookmark'
+require 'database_helpers'
 
 describe Bookmark do
 
@@ -7,23 +8,29 @@ describe Bookmark do
       connection = PG.connect(dbname: 'bookmark_manager_test')
 
       # Add the test data
-      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.youtube.com');")
+      bookmark = Bookmark.create(url: "http://www.makersacademy.com", title: "Makers")
+      Bookmark.create(url: "http://www.google.com", title: "Google")
+      Bookmark.create(url: "http://www.youtube.com", title: "Youtube")
 
       bookmarks = Bookmark.all
 
-      expect(bookmarks).to include("http://www.makersacademy.com")
-      expect(bookmarks).to include("http://www.google.com")
-      expect(bookmarks).to include("http://www.youtube.com")
+      expect(bookmarks.length).to eq 3
+      expect(bookmarks.first).to be_a Bookmark
+      expect(bookmarks.first.id).to eq bookmark.id
+      expect(bookmarks.first.title).to eq 'Makers'
+      expect(bookmarks.first.url).to eq 'http://www.makersacademy.com'
     end
   end
 
   describe '.create' do
     it 'creates a new bookmark' do
-      Bookmark.create(url: 'http://www.testbookmark.com')
+      bookmark = Bookmark.create(url: 'http://www.testbookmark.com', title: 'Test Bookmark')
+      persisted_data = persisted_data(id: bookmark.id)
 
-      expect(Bookmark.all).to include 'http://www.testbookmark.com'
+      expect(bookmark).to be_a Bookmark
+      expect(bookmark.id).to eq persisted_data['id']
+      expect(bookmark.title).to eq 'Test Bookmark'
+      expect(bookmark.url).to eq 'http://www.testbookmark.com'
     end
   end
 end
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,24 +1,24 @@
-feature 'visiting the homepage' do
-  scenario 'the page title is visible' do
-    visit '/'
-
-    expect(page).to have_content 'Bookmark Manager'
-  end
-end
-
 feature 'Viewing bookmarks' do
-  scenario 'A user can see bookmarks' do
-    connection = PG.connect(dbname: 'bookmark_manager_test')
+  feature 'visiting the homepage' do
+    scenario 'the page title is visible' do
+      visit '/'
 
-    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.google.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.youtube.com');")
+      expect(page).to have_content 'Bookmark Manager'
+    end
+  end
 
-    visit('/bookmarks')
+  feature 'Viewing bookmarks' do
+    scenario 'A user can see bookmarks' do
+      Bookmark.create(url: "http://www.makersacademy.com", title: "Makers")
+      Bookmark.create(url: "http://www.google.com", title: "Google")
+      Bookmark.create(url: "http://www.youtube.com", title: "Youtube")
 
-    expect(page).to have_content 'http://www.makersacademy.com'
-    expect(page).to have_content 'http://www.google.com'
-    expect(page).to have_content 'http://www.youtube.com'
+      visit('/bookmarks')
 
+      expect(page).to have_link('Makers', href: 'http://www.makersacademy.com')
+      expect(page).to have_link('Google', href: 'http://www.google.com')
+      expect(page).to have_link('Youtube', href: 'http://www.youtube.com')
+
+    end
   end
 end
